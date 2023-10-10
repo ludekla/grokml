9c925a7db210b7e65e82ed88066f91922f4eda2f
lutz <lutz@here.com>
2023-10-11 00:42:20 +0200

    Add comments to token pkg and change method into helper function.

--- pkg/tokens/tokens.go
+++ pkg/tokens/tokens.go
@@ -4,18 +4,24 @@ import (
 	"strings"
 )
 
+// TokenMap implements a mapping from tokens to floats. It acts as a vector with
+// a float parameter for every word, ie token, as obtained from text data, and all
+// the required canonical vector operations. 
 type TokenMap map[string]float64
 
+// New is a factory function for TokenMap objects.
 func New(size int) TokenMap {
 	return make(TokenMap, size)
 }
 
+// IAdd implements in-place addition with another token map.
 func (tm TokenMap) IAdd(other TokenMap) {
 	for token, val := range other {
 		tm[token] += val
 	}
 }
 
+// ScalMul implements scalar multiplication.
 func (tm TokenMap) ScaMul(factor float64) TokenMap {
 	otm := make(TokenMap)
 	for token, val := range tm {
@@ -24,6 +30,7 @@ func (tm TokenMap) ScaMul(factor float64) TokenMap {
 	return otm
 }
 
+// Dot implements the dot product against another token map.
 func (tm TokenMap) Dot(other TokenMap) float64 {
 	var sum float64
 	for token, val := range other {
@@ -32,32 +39,40 @@ func (tm TokenMap) Dot(other TokenMap) float64 {
 	return sum
 }
 
+// Tokeniser implements the Transformer interface.
 type Tokeniser struct {
-	ToLower bool
+	toLower bool
 }
 
+// NewTokeniser is a factory function for Tokenisers.
 func NewTokeniser(toLower bool) *Tokeniser {
 	return &Tokeniser{toLower}
 }
 
+// Transform is the defining method for Transformers: it takes a slice of 
+// string slices - construed as documents - and returns a token map for every
+// document. 
 func (t Tokeniser) Transform(docs [][]string) []TokenMap {
 	tmaps := make([]TokenMap, len(docs))
 	for i, doc := range docs {
 		tm := TokenMap{}
 		for _, txt := range doc {
-			tm.IAdd(t.TokenFreqs(txt))
+			tm.IAdd(TokenFreqs(txt, t.toLower))
 		}
 		tmaps[i] = tm
 	}
 	return tmaps
 }
 
-func (t Tokeniser) TokenFreqs(txt string) TokenMap {
+// TokenFreqs is a helper function that tokenises a string, computes
+// their relative frequencies with the string and stores them inside
+// a token map which is then returned.
+func TokenFreqs(txt string, toLower bool) TokenMap {
 	tmap := make(TokenMap)
 	tokens := strings.Split(txt, " ")
 	var total float64
 	for _, token := range tokens {
-		if t.ToLower {
+		if toLower {
 			token = strings.ToLower(token)
 		}
 		tmap[token] += 1.0
